510be99096dbdc16089d92a2ec6ec909d368480e
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-11-17 11:44:26 +0530

    Craete contact

--- AdressBookSystem/AdressBookSystem/Program.cs
+++ AdressBookSystem/AdressBookSystem/Program.cs
@@ -6,8 +6,29 @@ namespace AdressBookSystem
     {
             static void Main(string[] args)
             {
-                Console.WriteLine("Welcome to Adddress Book");
 
-            }
+            Console.WriteLine("Welcome To adress Book System!");
+            Program program = new Program();
+            Console.WriteLine("Enter first name = ");
+            string fName = Console.ReadLine();
+            Console.WriteLine("Enter last name = ");
+            string lName = Console.ReadLine();
+            Console.WriteLine("Enter address= ");
+            String address = Console.ReadLine();
+            Console.WriteLine("Enter city= ");
+            String city = Console.ReadLine();
+            Console.WriteLine("Enter state= ");
+            String state = Console.ReadLine();
+            Console.WriteLine("Enter zip= ");
+            String zip = Console.ReadLine();
+            Console.WriteLine("Enter phoneNumber= ");
+            String phoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter email= ");
+            String email = Console.ReadLine();
+
+            AdressBookBuilder adressBookBuilder = new AdressBookBuilder();
+            adressBookBuilder.AddContact(fName, lName, address, city, state, zip, phoneNumber, email);
+            adressBookBuilder.displayContact();
+        }
     }
 }
